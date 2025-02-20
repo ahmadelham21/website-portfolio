a7b332d8814b217b75893fb28622b33868477407
Ahmad Ilham <158115636+ahmadelham21@users.noreply.github.com>
2025-02-20 15:54:53 +0700

    Update Hero.jsx

--- src/pages/Home/Components/Hero.jsx
+++ src/pages/Home/Components/Hero.jsx
@@ -21,9 +21,6 @@ const Hero = () => {
       <Typography variant="h5" component="h2" gutterBottom>
         I'm a Full Stack Developer @ Maybank Finance
       </Typography>
-      <Typography variant="h5" component="h2" gutterBottom>
-        KAK BAGAS KEREN
-      </Typography>
       <Button variant="contained" color="primary" size="large">
         View My Work
       </Button>
